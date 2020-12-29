3560970b3f4943003317f064af774ebcbf14272f
VirajS00 <virajshukla00@gmail.com>
2020-12-29 17:18:55 +0530

    changed selection colour and fixed nav on mobile

--- js/nav.js
+++ js/nav.js
@@ -2,5 +2,18 @@ const nav_ul = document.querySelector('.nav-ul');
 const nav = document.querySelector('nav');
 
 nav.addEventListener('click', (e) => {
-	nav_ul.classList.toggle('active');
+	e.stopPropagation();
+	if (e.currentTarget === nav) {
+		nav_ul.classList.add('active');
+	} else {
+		nav_ul.classList.remove('active');
+	}
+	console.log(e.currentTarget);
+	// nav_ul.classList.toggle('active');
+});
+
+window.addEventListener('click', (e) => {
+	if (e.currentTarget !== nav) {
+		nav_ul.classList.remove('active');
+	}
 });
