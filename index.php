de2a06f8ea264aa804905c37808f7dc7d0b60b70
VirajS00 <virajshukla00@gmail.com>
2021-06-13 18:57:14 +0530

    made changes to home page

--- index.php
+++ index.php
@@ -39,15 +39,21 @@
 		<nav style="background-color: transparent">
 			<img src="images/logo.svg" alt="logo" class="nav-img" />
 			<ul class="nav-ul">
-				<li class="nav-li"><a href="index.php" class="nav-link active">Home</a></li>
-				<li class="nav-li"><a href="photoCateg.php" class="nav-link">Photos</a></li>
+				<li class="nav-li">
+					<a href="index.php" class="nav-link active">Home</a>
+				</li>
+				<li class="nav-li">
+					<a href="photoCateg.php" class="nav-link">Photos</a>
+				</li>
 				<li class="nav-li">
 					<a href="films.php" class="nav-link">Filmmaking</a>
 				</li>
 				<li class="nav-li">
 					<a href="other.php" class="nav-link">Coding and Design</a>
 				</li>
-				<li class="nav-li"><a href="contact.php" class="nav-link">Contact</a></li>
+				<li class="nav-li">
+					<a href="contact.php" class="nav-link">Contact</a>
+				</li>
 				<li class="nav-li"><a href="about.php" class="nav-link">About</a></li>
 			</ul>
 		</nav>
@@ -57,10 +63,16 @@
 				<span style="font-size: 2.5rem"
 					>I’m <span style="color: #b2c1ff">Viraj Shukla</span></span
 				>
-				<span style="margin-top: 20px; font-size: 1.35rem; text-align: center; margin-bottom: 2rem;">
-					<a href="photoCateg.php" class="home-link">Photography</a> | <a href="films.php" class="home-link">Filmmaking</a> | <a href="other.php" class="home-link">Web Development</a>
-				</span>
-				<a href="contact.php" class="contact-link">Contact Me</a>
+				<a href="contact.php" class="contact-link" style="margin-top: 1.5rem"
+					>Contact Me</a
+				>
+				<a class="arr-link" href="#">
+					<img src="images/down_arr.svg" />
+				</a>
+			</section>
+			<section class="home_page photo_section">
+				<h1 class="section-title">Photography</h1>
+				<a href="photoCateg.php" class="explore-button">Explore</a>
 			</section>
 		</main>
 		<script src="js/nav.js"></script>
