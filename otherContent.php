7ea2baaf3a7f289d1c1be1aa06c4581a9d19338c
VirajS00 <virajshukla00@gmail.com>
2021-08-22 18:26:01 +0530

    changed design for otherContent page

--- otherContent.php
+++ otherContent.php
@@ -12,9 +12,15 @@
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title><?php echo $data->title; ?></title>
-		<link rel="stylesheet" href="css/otherContent.min.css" />
+		<link rel="stylesheet" href="css/otherContent.min.css?V=1" />
 		<link rel="shortcut icon" href="images/favicon.ico" />
 		<link rel="apple-touch-icon" href="images/favicon.png" />
+		<style>
+			pre {
+				--extra-width-pre: 100px;
+				width: calc(100% + var(--extra-width-pre));
+			}
+		</style>
 	</head>
 	<body>
 		<main>
@@ -57,36 +63,39 @@
 						</svg>
 					</a>
 				</div>
-				<h1 class="title"><?php echo strtoupper($data->title); ?></h1>
+				<h1 class="title-top"><?php echo strtoupper($data->title); ?></h1>
 			</section>
-			<section class="content-top">
-				<article class="top-text" style="padding: 0 20px;">
-					<p class="content">
-						<?php echo $data->short_desc ?>
-					</p>
-				</article>
-				<article class="top-videoimg">
-					<div class="video-container">
-						<?php echo $data->video_img ?>
+			<div class="wrapper">
+				<section class="content-top">
+					<article class="top-videoimg">
+						<div class="video-container">
+							<?php echo $data->video_img ?>
+						</div>
+					</article>
+					<article class="top-text">
+						<p class="content">
+							<?php echo $data->short_desc ?>
+						</p>
+					</article>
+				</section>
+				<section class="main-content">
+					<h1 class="title">PROJECT DETAILS</h1>
+					<?php echo $data->description; ?>
+					<div class="platform">
+						<h1 class="title">PLATFORM USED</h1>
+						<div class="platFormImgC">
+							<?php
+								$platforms = $data->platform;
+								$q1 = 'SELECT img_code FROM platforms WHERE id IN ('.$platforms.')';
+								$r1 = mysqli_query($conn, $q1);
+								while($row = mysqli_fetch_array($r1)) {
+									echo $row['img_code'];
+								}
+							?>
+						</div>
 					</div>
-				</article>
-			</section>
-			<section class="main-content">
-				<?php echo $data->description; ?>
-				<div class="platform">
-					<h1 class="title">PLATFORM USED</h1>
-					<div class="platFormImgC">
-						<?php
-							$platforms = $data->platform;
-							$q1 = 'SELECT img_code FROM platforms WHERE id IN ('.$platforms.')';
-							$r1 = mysqli_query($conn, $q1);
-							while($row = mysqli_fetch_array($r1)) {
-								echo $row['img_code'];
-							}
-						?>
-					</div>
-				</div>
-			</section>
+				</section>
+			</div>
 		</main>
 		<script src="js/prism.js"></script>
 		<script src="js/darkmode.js"></script>
@@ -136,6 +145,12 @@
 			linkTag.rel = 'stylesheet';
 			linkTag.href = link;
 			document.body.appendChild(linkTag);
+
+			if(!!document.querySelector('pre')) {
+				const styles = window.getComputedStyle(document.querySelector('pre'));
+				let margin = (parseInt(styles.getPropertyValue('--extra-width-pre')) / 2) * -1;
+				document.querySelector('pre').style.marginLeft = `${margin}px`;
+			}
 		</script>
 	</body>
 </html>
