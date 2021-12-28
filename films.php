4ffe688071a97892e0bf45351e68b0f6962ad63a
Viraj Shukla <virajshukla@Virajs-MacBook-Air.local>
2021-12-28 20:05:57 +0530

    added roles to films page

--- films.php
+++ films.php
@@ -4,6 +4,15 @@
         $regex = '/(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/im';
         return preg_replace($regex, '<a href="$1" target="_blank" class="linkFilm">$1</a>', $string);
 	}
+
+	function sortArrayByKey(array &$array, array $orderArray) {
+		$ordered = [];
+		for($i = 0; $i < count($orderArray); $i++) {
+			$ordered[$orderArray[$i]] = $array[$orderArray[$i]];
+		}
+
+		return $ordered;
+	}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,7 +22,7 @@
 		<title>Filmmaking</title>
 		<meta name="description" content="My name is Viraj Shukla and I'm a film student, here are some of my college film projects" />
 		<meta name="KeyWords" content="Viraj Shukla, website, personal website, portfolio, filmmaking">
-		<link rel="stylesheet" href="css/style.min.css?v=1" />
+		<link rel="stylesheet" href="css/style.min.css?v=5" />
 		<link rel="shortcut icon" href="images/favicon.ico" />
 		<link rel="apple-touch-icon" href="images/favicon.png" />
 		<style>
@@ -26,33 +35,71 @@
 		<div class="embedLinks" id="embedLinks">
 			<?php
 				$key = YOUR_KEY_HERE;
-				$url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=PLNUNNqPwkQe-67Wlv8WkoK7fZO96I07wf&access_token=AIzaSyBY5Fyycf5sZ9CWDFMZjrDSw32MW0i9SGc&key=$key";
+				$token = YOUR_ACCESS_TOKEN_HERE;
+				$url = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=20&playlistId=PLNUNNqPwkQe-67Wlv8WkoK7fZO96I07wf&access_token=$token&key=$key";
                 $client = curl_init($url);
 				curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
 				$response = curl_exec($client);
                 $jsonArr = json_decode($response, true);
 				$items = $jsonArr['items'];
+				$video_ids = [];
+
 				for ($i=0; $i < sizeof($items); $i++) {
-					$videoId = $items[$i]['snippet']['resourceId']['videoId'];
-					$a = $i + 1;
-					$b = $a - 1;
-					$c = $a + 1;
-					echo "<div id='film$a' class='filmE'>";
-					echo "<div class='left-side'><div class='video-container'>";
-					echo "<iframe width='800' height='491' src='https://www.youtube.com/embed/$videoId' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
-					echo "</div></div>";
-					echo "<div class='right-side'><a href='#!' class='close1'>&times;</a>";
-					echo "<h1 class='title'>".strtoupper($items[$i]['snippet']['title'])."</h1>";
-					echo "<p class='desc'>".nl2br(addATag($items[$i]['snippet']['description']))."</p>";
-					echo "<div class='arrowsGridInside'>";
-					echo "<a href='#film$b'><svg width='62' height='62' viewBox='0 0 62 62' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M43 6L18 31L43 56' style='stroke: var(--arr-c); transition: stroke 250ms ease;' stroke-width='4'/></svg>
-						</a><a href='#film$c'><svg width='62' height='62' viewBox='0 0 62 62' fill='none' xmlns='http://www.w3.org/2000/svg'>
-						<path d='M18 6L43 31L18 56' style='stroke: var(--arr-c); transition: stroke 250ms ease;' stroke-width='4'/>
-						</svg></a>";
-					echo "</div>";
-					echo "</div>";
-					echo "</div>";
+					$id = $items[$i]['id'];
+					array_push($video_ids, $id);
 				}
+
+				include('php/connect.php');
+
+				$q = 'SELECT * FROM films';
+				$r = mysqli_query($conn, $q);
+
+				if($r) {
+					$a = 0;
+					$roles = [];
+					$film_ids_db = [];
+					while($row = mysqli_fetch_array($r)) {
+						// add film id as index in array and roles as value
+						$film_ids_db[$row['film_id']] = $row['my_role'];
+					}
+
+					$sorted = sortArrayByKey($film_ids_db, $video_ids);
+
+					for($i = 0; $i < count($sorted); $i++) {
+						$id = $items[$i]['id'];
+						$thumbnail = $items[$i]['snippet']['thumbnails']['medium']['url'];
+                    	$title = $items[$i]['snippet']['title'];
+						$videoId = $items[$i]['snippet']['resourceId']['videoId'];
+
+						$a = $i + 1;
+						$b = $a - 1;
+						$c = $a + 1;
+						echo "<div id='film$a' class='filmE'>";
+						echo "<div class='left-side'><div class='video-container'>";
+						echo "<iframe width='800' height='491' src='https://www.youtube.com/embed/$videoId' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>";
+						echo "</div></div>";
+						echo "<div class='right-side'><a href='#!' class='close1'>&times;</a>";
+						echo "<div class='title_id'>";
+						echo "<h1 class='title'>".strtoupper($items[$i]['snippet']['title'])."</h1>";
+						echo "<p class='role'>My Role: ".$sorted[$id]."</p>";
+						echo "</div>";
+						echo "<p class='desc'>".nl2br(addATag($items[$i]['snippet']['description']))."</p>";
+						echo "<div class='arrowsGridInside'>";
+						echo "<a href='#film$b'><svg width='62' height='62' viewBox='0 0 62 62' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M43 6L18 31L43 56' style='stroke: var(--arr-c); transition: stroke 250ms ease;' stroke-width='4'/></svg>
+							</a><a href='#film$c'><svg width='62' height='62' viewBox='0 0 62 62' fill='none' xmlns='http://www.w3.org/2000/svg'>
+							<path d='M18 6L43 31L18 56' style='stroke: var(--arr-c); transition: stroke 250ms ease;' stroke-width='4'/>
+							</svg></a>";
+						echo "</div>";
+						echo "</div>";
+						echo "</div>";
+
+						echo '</div>';
+					}
+
+				} else {
+					echo "MySQL Error: ".mysqli_error($conn);
+				}
+
 			?>
 		</div>
 		<nav>
@@ -79,14 +126,18 @@
 			<h1 class="title-about title-cd">FILMMAKING</h1>
 			<section class="films-grid">
 				<?php
-					for ($i=0; $i < sizeof($items); $i++){
+					for ($i = 0; $i < count($sorted); $i++){
+						$id = $items[$i]['id'];
 						$thumbnail = $items[$i]['snippet']['thumbnails']['medium']['url'];
                         $title = $items[$i]['snippet']['title'];
 						$b = $i + 1;
 						echo "<div class='filmThumb'>";
 						echo "<a href='#film$b' class='filmL'>";
 						echo "<img height='0' width='0' src='$thumbnail' alt='Njaan Karshakan' class='filmThumbImg' />";
-						echo "<div class='filmTitle'>$title</div>";
+						echo "<div class='filmTitle'>";
+						echo "<p class='title'>$title</p>";
+						echo "<p class='role'>My Role: ".$sorted[$id]."</p>";
+						echo "</div>";
 						echo "</a>";
 						echo "</div>";
 					}
